7539144895c84249710fa822d9d7554d51be07f1
Jared Forsyth <jared@jaredforsyth.com>
2020-05-15 09:47:54 -0600

    respect git attributes

--- dist/index.js
+++ dist/index.js
@@ -989,6 +989,63 @@ const path = __webpack_require__(622);
 const fs = __webpack_require__(747);
 const minimatch = __webpack_require__(93);
 
+// TODO respect .gitattributes files! If it's set to "linguist-generated=true" or "binary", exclude it.
+
+const getIgnoredPatterns = (fileContents) => {
+    return fileContents
+        .split('\n')
+        .map((line) => {
+            if (line.startsWith('#')) {
+                return null;
+            }
+            if (line.startsWith('"')) {
+                throw new Error('Quoted patterns not yet supported, sorry');
+            }
+            if (!line.trim()) {
+                return null;
+            }
+            const [pattern, ...attributes] = line.trim().split(' ');
+            if (
+                attributes.includes('binary') ||
+                attributes.inludes('linguist-generated=true')
+            ) {
+                return pattern;
+            }
+            return null;
+        })
+        .filter(Boolean);
+};
+
+const ignoredPatternsByDirectory = {};
+const isFileIgnored = (workingDirectory, file) => {
+    // If it's outside of the "working directory", we ignore it
+    if (!file.startsWith(workingDirectory)) {
+        return true;
+    }
+    let dir = path.dirname(file);
+    let name = path.basename(file);
+    while (dir.startsWith(workingDirectory)) {
+        if (!ignoredPatternsByDirectory[dir]) {
+            const attributes = path.join(dir, '.gitattributes');
+            if (fs.existsSync(attributes)) {
+                ignoredPatternsByDirectory[dir] = getIgnoredPatterns(
+                    fs.readFileSync(attributes, 'utf8'),
+                );
+            } else {
+                ignoredPatternsByDirectory[dir] = [];
+            }
+        }
+        for (const pattern of ignoredPatternsByDirectory[dir]) {
+            if (minimatch(pattern, name)) {
+                return true;
+            }
+        }
+        name = path.join(path.basename(dir), name);
+        dir = path.dirname(dir);
+    }
+    return false;
+};
+
 const ignore = getInput('ignore');
 const include = getInput('include');
 const base = getInput('base') || process.env.GITHUB_BASE_REF;
@@ -1014,11 +1071,13 @@ const files = stdout
     .map((name) => path.join(cwd, name))
     // Filter out paths that were deleted
     .filter((path) => fs.existsSync(path))
+    .filter((path) => !isFileIgnored(cwd, path))
     .filter((path) => (ignore ? !minimatch(path, ignore) : true))
     .filter((path) => (include ? minimatch(path, include) : true));
 
 console.log('Files changed:');
 files.forEach((file) => console.log('  ' + file));
+
 // ':' is an illegal character in filenames, so it makes a good delimeter
 setOutput('files', files.join(':::'));
 
--- index.js
+++ index.js
@@ -4,6 +4,61 @@ const path = require('path');
 const fs = require('fs');
 const minimatch = require('minimatch');
 
+const getIgnoredPatterns = (fileContents) => {
+    return fileContents
+        .split('\n')
+        .map((line) => {
+            if (line.startsWith('#')) {
+                return null;
+            }
+            if (line.startsWith('"')) {
+                throw new Error('Quoted patterns not yet supported, sorry');
+            }
+            if (!line.trim()) {
+                return null;
+            }
+            const [pattern, ...attributes] = line.trim().split(' ');
+            if (
+                attributes.includes('binary') ||
+                attributes.inludes('linguist-generated=true')
+            ) {
+                return pattern;
+            }
+            return null;
+        })
+        .filter(Boolean);
+};
+
+const ignoredPatternsByDirectory = {};
+const isFileIgnored = (workingDirectory, file) => {
+    // If it's outside of the "working directory", we ignore it
+    if (!file.startsWith(workingDirectory)) {
+        return true;
+    }
+    let dir = path.dirname(file);
+    let name = path.basename(file);
+    while (dir.startsWith(workingDirectory)) {
+        if (!ignoredPatternsByDirectory[dir]) {
+            const attributes = path.join(dir, '.gitattributes');
+            if (fs.existsSync(attributes)) {
+                ignoredPatternsByDirectory[dir] = getIgnoredPatterns(
+                    fs.readFileSync(attributes, 'utf8'),
+                );
+            } else {
+                ignoredPatternsByDirectory[dir] = [];
+            }
+        }
+        for (const pattern of ignoredPatternsByDirectory[dir]) {
+            if (minimatch(name, pattern)) {
+                return true;
+            }
+        }
+        name = path.join(path.basename(dir), name);
+        dir = path.dirname(dir);
+    }
+    return false;
+};
+
 const ignore = getInput('ignore');
 const include = getInput('include');
 const base = getInput('base') || process.env.GITHUB_BASE_REF;
@@ -29,10 +84,12 @@ const files = stdout
     .map((name) => path.join(cwd, name))
     // Filter out paths that were deleted
     .filter((path) => fs.existsSync(path))
+    .filter((path) => !isFileIgnored(cwd, path))
     .filter((path) => (ignore ? !minimatch(path, ignore) : true))
     .filter((path) => (include ? minimatch(path, include) : true));
 
 console.log('Files changed:');
 files.forEach((file) => console.log('  ' + file));
+
 // ':' is an illegal character in filenames, so it makes a good delimeter
 setOutput('files', files.join(':::'));
